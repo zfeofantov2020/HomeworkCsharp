4b10b1a2a3240c7c774fe615420895313ea4d420
Eugene <zfeofantov@gmail.com>
2022-09-23 21:47:02 +0100

    Secont Task to Complet

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-// a = 5; b = 7 -> max = 7 a = 2 b = 10 -> max = 10 a = -9 b = -3 -> max = -3
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
 Console.Clear();
 Console.WriteLine("Task number 2");
@@ -13,7 +15,7 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 
 if(num1 > num2)
     {
-        Console.WriteLine($"First number {num1} greater than second number {num2}");
+        Console.WriteLine($"First number {num1} greater than Second number {num2}");
     }
 else if(num1 < num2)
     {
@@ -23,3 +25,5 @@ else
     {
         Console.WriteLine($"First number {num1} equal to Second number {num2}");
     }
+
+
