a26781dbe6dd28b2c550209addbf5d6e34d57922
Eugene <zfeofantov@gmail.com>
2022-09-23 23:48:10 +0100

    Start fourth task

--- Program.cs
+++ Program.cs
@@ -2,24 +2,30 @@
 
 // a = 5; b = 7 -> max = 7 a = 2 b = 10 -> max = 10 a = -9 b = -3 -> max = -3
 
-Console.Clear();
-Console.WriteLine("Task number 2");
+// Console.Clear();
+// Console.WriteLine("Task number 2");
 
-Console.WriteLine("Insert First Number");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Insert First Number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Insert Second Number");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Insert Second Number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if(num1 > num2)
-    {
-        Console.WriteLine($"First number {num1} greater than second number {num2}");
-    }
-else if(num1 < num2)
-    {
-        Console.WriteLine($"Second nubmer {num2} greater then {num1} ");
-    }
-else
-    {
-        Console.WriteLine($"First number {num1} equal to Second number {num2}");
-    }
+// if(num1 > num2)
+//     {
+//         Console.WriteLine($"First number {num1} greater than second number {num2}");
+//     }
+// else if(num1 < num2)
+//     {
+//         Console.WriteLine($"Second nubmer {num2} greater then {num1} ");
+//     }
+// else
+//     {
+//         Console.WriteLine($"First number {num1} equal to Second number {num2}");
+//     }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
